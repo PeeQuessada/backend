fd563d10ae64a85c54e98dc721fe83f4b6345e8b
Pedro Quessada <pedro.rafael.quessada@gmail.com>
2024-02-15 22:07:04 -0300

    feat: create endpoints for CRUD

--- app/app.js
+++ app/app.js
@@ -1,16 +1,69 @@
-var express = require("express");
-var app = express();
-var port = process.env.PORT || 3000;
+const express = require("express");
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-});
+const app = express();
+app.use(express.json());
 
-app.get("/", (req, res, next) => {
-    res.json(`{ code: 200, message: 'sucesso', port: ${port} }`
-    );
-});
+const fakeBD = [];
 
 app.get("/get", (req, res, next) => {
-    res.json(["teste", "teste2"]);
+    res.status(200).json(fakeBD);
+});
+
+app.post("/create", (req, res, next) => {
+    try {
+        const newRecord = req.body;
+
+        if(!newRecord) {
+            return res.status(400).json({message: "Error"});
+        }
+    
+        fakeBD.push(newRecord);
+    
+        res.status(200).json({message: "record created successfully"});
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+});
+
+app.put("/update/:id", (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const index = id - 1;
+        if(fakeBD.length <= index|| id == 0) {
+            return res.status(400).json({message: "Error invalid ID"});
+        }
+    
+        const newRecord = req.body;
+        if(!newRecord) {
+            return res.status(400).json({message: "Error"});
+        }
+    
+        fakeBD[index] = newRecord;
+    
+        res.status(200).json({message: "Record updated successfully"});
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+});
+
+app.delete("/delete/:id", (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const index = id - 1;
+
+        if(fakeBD.length <= index || id == 0) {
+            return res.status(400).json({message: "Error invalid ID"});
+        }
+    
+        fakeBD.splice(index, 1);
+    
+        res.status(200).json({message: "Record deleted successfully"});
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
