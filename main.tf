9d2b0ded77ac0971de78f6b2d5f1f019353b0942
Pedro Quessada <pedro.rafael.quessada@gmail.com>
2024-02-17 23:30:32 -0300

    feat: add node

--- main.tf
+++ main.tf
@@ -26,17 +26,37 @@ resource "aws_vpc" "eks_vpc" {
   }
 }
 
+# Define as zonas de disponibilidade
+variable "availability_zones" {
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+
 # Recurso para criar subnets privadas
 resource "aws_subnet" "private_subnet" {
-  count = 2
+  count = length(var.availability_zones)
 
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.${count.index + 1}.0/24"
-  availability_zone       = "us-east-1"  # Substitua pela AZ desejada
+  availability_zone       = element(var.availability_zones, count.index)
   map_public_ip_on_launch = false
 
   tags = {
     Name = "private-subnet-${count.index + 1}"
+  }
+}
+
+# Recurso para criar uma subnet pública
+resource "aws_subnet" "public_subnet" {
+  count = length(var.availability_zones)
+
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.${count.index + 3}.0/24" 
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index}"
   }
 }
 
@@ -74,13 +94,54 @@ resource "aws_eks_cluster" "my_cluster" {
   depends_on = [aws_subnet.private_subnet]
 }
 
+resource "aws_eks_node_group" "node" {
+    cluster_name = aws_eks_cluster.my_cluster.name
+    node_group_name = "my-backend-node"
+    node_role_arn = "arn:aws:iam::211125361403:role/LabRole" 
+    subnet_ids = aws_subnet.private_subnet[*].id
+    scaling_config {
+        desired_size = 2
+        max_size = 4
+        min_size = 2
+    }
+    depends_on = [ 
+        # policies
+    ]
+    instance_types = ["t3.micro"]
+}
+
+# Recurso para criar o Internet Gateway
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+# Recurso para criar uma nova Route Table e associar à subnet pública
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+}
+
+# Recurso para associar a nova Route Table à subnet pública
+resource "aws_route_table_association" "public_route_association" {
+  count          = length(aws_subnet.public_subnet)
+
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
 # Recurso para criar um LoadBalancer público
 resource "aws_lb" "my_lb" {
   name               = "my-loadbalancer"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = aws_subnet.private_subnet[*].id
+  subnets            = aws_subnet.public_subnet[*].id  # Use a subnet pública para o Load Balancer
+
+  enable_deletion_protection = false
 }
 
 # Recurso para criar um Security Group para o LoadBalancer
