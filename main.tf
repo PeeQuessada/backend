7e42312e212b457884a16fe2b856d881880acce1
Pedro Quessada <pedro.rafael.quessada@gmail.com>
2024-02-19 13:29:45 -0300

    feat: refactoring main.tf

--- main.tf
+++ main.tf
@@ -1,244 +1,26 @@
 # main.tf
-#Configuração do provedor AWS
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+module "infra" {
+    source = "./setupInfra" 
 
-# Configure AWWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
+    region = "us-east-1"
 
+    prefix = "tf"
+    repository_name = "backend"
+    cluster_name = "cluster"
+    image = "pedroquessada/my-backend:latest"
 
-# Recurso para criar a VPC
-resource "aws_vpc" "eks_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
+    role = "arn:aws:iam::211125361403:role/LabRole"
+    profile = ""
 
-  tags = {
-    Name = "eks-vpc"
-  }
-}
-
-#############################################
-
-# Define as zonas de disponibilidade
-variable "availability_zones" {
-  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
-
-# Recurso para criar uma subnet pública
-resource "aws_subnet" "public_subnet" {
-  count = length(var.availability_zones)
-
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.${count.index + 1}.0/24" 
-  availability_zone       = element(var.availability_zones, count.index)
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-${count.index}"
-  }
-}
-
-# Recurso para criar o Internet Gateway
-resource "aws_internet_gateway" "eks_igw" {
-  vpc_id = aws_vpc.eks_vpc.id
-}
-
-# Recurso para criar uma nova Route Table e associar à subnet pública
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.eks_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.eks_igw.id
-  }
-}
-
-# Recurso para associar a nova Route Table à subnet pública
-resource "aws_route_table_association" "public_route_association" {
-  count          = length(aws_subnet.public_subnet)
-
-  subnet_id      = aws_subnet.public_subnet[count.index].id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-############################################
-
-# Recurso para criar o cluster EKS
-resource "aws_eks_cluster" "my_cluster" {
-  name     = "my-eks-cluster"
-  role_arn = "arn:aws:iam::211125361403:role/LabRole" #aws_iam_role.eks_cluster.arn
-  vpc_config {
-    subnet_ids = aws_subnet.public_subnet[*].id
-  }
-  depends_on = [aws_subnet.public_subnet]
-}
-
-# Recurso para criar um no em cada subrede
-resource "aws_eks_node_group" "node" {
-    cluster_name = aws_eks_cluster.my_cluster.name
-    node_group_name = "my-node"
-    node_role_arn = "arn:aws:iam::211125361403:role/LabRole" 
-    subnet_ids = aws_subnet.public_subnet[*].id
-    scaling_config {
-        desired_size = 2
-        max_size = 4
-        min_size = 2
-    }
-    depends_on = [ 
-        # policies
-    ]
-    instance_types = ["t2.micro"]
-}
-
-#####################################
-
-# Recurso para criar um LoadBalancer público
-# resource "aws_lb" "my_lb" {
-#   name               = "my-loadbalancer"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.lb_sg.id]
-#   subnets            = aws_subnet.public_subnet[*].id 
-
-#   enable_deletion_protection = false
-# }
-
-# # Recurso para criar um Security Group para o LoadBalancer
-# resource "aws_security_group" "lb_sg" {
-#   vpc_id = aws_vpc.eks_vpc.id
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-# # # Liberar tráfego para o LoadBalancer
-# resource "aws_security_group_rule" "lb_ingress" {
-#   security_group_id = aws_security_group.lb_sg.id
-#   type              = "ingress"
-#   from_port         = 3000
-#   to_port           = 3000
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
-
-# Deploy k8s
-# Configuração do provedor Kubernetes
-
-provider "kubernetes" {
-  # config_path = "~/.kube/config"
-  host                   = aws_eks_cluster.my_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.my_cluster.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", "my-eks-cluster"]
-    command     = "aws"
-  }
-}
-
-resource "kubernetes_deployment" "Application" {
-  metadata {
-    name = "my-backend"
-    labels = {
-      nome = "my-backend"
-    }
-  }
-
-  spec {
-    replicas = 2
-
-    selector {
-      match_labels = {
-        nome = "my-backend"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          nome = "my-backend"
-        }
-      }
-
-      spec {
-        container {
-          image = "pedroquessada/my-backend:latest"
-          name  = "my-backend"
-
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-
-          liveness_probe {
-            http_get {
-              path = "/"
-              port = 3000
-            }
-
-            initial_delay_seconds = 600
-            period_seconds        = 300
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "LoadBalancer" {
-  metadata {
-    name = "my-backend"
-  }
-  spec {
-    selector = {
-      nome = "my-backend"
-    }
-    port {
-      port        = 3000
-      target_port = 3000
-    }
-    type = "LoadBalancer"
-  }
-}
-
-
-# Create a local variable for the load balancer name.
-locals {
-  lb_name = split("-", split(".", kubernetes_service.LoadBalancer.status.0.load_balancer.0.ingress.0.hostname).0).0
-}
-
-# Read information about the load balancer using the AWS provider.
-data "aws_elb" "LoadBalancer" {
-  name = local.lb_name
-}
-
-output "load_balancer_name" {
-  value = local.lb_name
+    access_key = ""
+    secret_key = ""
+    session_token = ""
 }
 
 output "load_balancer_hostname" {
-  value = kubernetes_service.LoadBalancer.status.0.load_balancer.0.ingress.0.hostname
+  value = module.infra.lb_hostname
 }
 
 output "load_balancer_info" {
-  value = data.aws_elb.LoadBalancer
+  value = module.infra.lb_info
 }
---
+++ setupInfra/Kubernetes.tf
@@ -0,0 +1,87 @@
+resource "kubernetes_deployment" "Application" {
+  metadata {
+    name = "${var.prefix}-${var.repository_name}"
+    labels = {
+      nome = "${var.prefix}-${var.repository_name}"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        nome = "${var.prefix}-${var.repository_name}"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          nome = "${var.prefix}-${var.repository_name}"
+        }
+      }
+
+      spec {
+        container {
+          image = var.image
+          name  = "${var.prefix}-${var.repository_name}"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 3000
+            }
+
+            initial_delay_seconds = 600
+            period_seconds        = 300
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "LoadBalancer" {
+  metadata {
+    name = "${var.prefix}-${var.repository_name}"
+  }
+  spec {
+    selector = {
+      nome = "${var.prefix}-${var.repository_name}"
+    }
+    port {
+      port        = 3000
+      target_port = 3000
+    }
+    type = "LoadBalancer"
+  }
+}
+
+
+locals {
+  lb_name = split("-", split(".", kubernetes_service.LoadBalancer.status.0.load_balancer.0.ingress.0.hostname).0).0
+}
+
+data "aws_elb" "LoadBalancer" {
+  name = local.lb_name
+}
+
+output "lb_hostname" {
+  value = local.lb_name
+}
+
+output "lb_info" {
+  value = data.aws_elb.LoadBalancer
+}
