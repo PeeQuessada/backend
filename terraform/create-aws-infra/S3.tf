c169c8c7ab5aa32637f937a20807e4173911d324
Pedro Quessada <pedro.rafael.quessada@gmail.com>
2024-02-22 02:36:35 -0300

    build

--- terraform/create-aws-infra/S3.tf
+++ terraform/create-aws-infra/S3.tf
@@ -1,16 +1,16 @@
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "s3bucket" {
   bucket = "${var.prefix}-${var.repository_name}-${var.image_version}"
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_ownership_controls" "s3bucket" {
+  bucket = aws_s3_bucket.s3bucket.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_public_access_block" "s3bucket" {
+  bucket = aws_s3_bucket.s3bucket.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -18,16 +18,16 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "bucket" {
+resource "aws_s3_bucket_acl" "s3bucket" {
   depends_on = [
-    aws_s3_bucket_ownership_controls.bucket,
-    aws_s3_bucket_public_access_block.bucket,
+    aws_s3_bucket_ownership_controls.s3bucket,
+    aws_s3_bucket_public_access_block.s3bucket,
   ]
 
-  bucket = aws_s3_bucket.bucket.id
+  bucket = aws_s3_bucket.s3bucket.id
   acl    = "public-read"
 }
 
 output "s3_bucket_uri" {
-  value = aws_s3_bucket.bucket.bucket
+  value = aws_s3_bucket.s3bucket.bucket
 }
