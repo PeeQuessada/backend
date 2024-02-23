55dd98a90690527c6a475c0af2796932af8b3072
PedroQuessada <pedro.rafael.quessada@gmail.com>
2024-02-23 19:43:47 -0300

    update k8s

--- terraform/deploy-k8s/Kubernetes.tf
+++ terraform/deploy-k8s/Kubernetes.tf
@@ -19,6 +19,14 @@ resource "kubernetes_deployment" "Application" {
   spec {
     replicas = 2
 
+    strategy {
+      type = "RollingUpdate"
+      rolling_update {
+        max_surge       = 1
+        max_unavailable = 1
+      }
+    }
+
     selector {
       match_labels = {
         nome = "${var.prefix}-${var.repository_name}"
