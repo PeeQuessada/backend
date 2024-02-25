c0daef52e66973765c56af1a015ad85d84f982f6
PedroQuessada <pedro.rafael.quessada@gmail.com>
2024-02-25 19:02:33 -0300

    update gcp workflow

--- terraform/gcp/create-gcp-infra/Variables.tf
+++ terraform/gcp/create-gcp-infra/Variables.tf
@@ -1,30 +1,24 @@
 # Define as zonas de disponibilidade
 variable "region" {
   type    = string
-  default = "us-central1"
 }
 
 variable "zone" {
   type    = string
-  default = "us-central1-f"
 }
 
 variable "bucket_name" {
   type    = string
-  default = "us-central1-my-project-buckey-3"
 }
 
 variable "prefix" {
   type    = string
-  default = "tf"
 }
 
 variable "project_id" {
   type    = string
-  default = "backend-test1-415414"
 }
 
 variable "image_version" {
   type    = string
-  default = "test"
 }
