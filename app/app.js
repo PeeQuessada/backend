adde24c3a1f8c1942082c5ca526a3d3896c248f3
PedroQuessada <pedro.rafael.quessada@gmail.com>
2024-02-25 23:19:33 -0300

    change text

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 const fakeBD = [];
 
 app.get("/", (req, res, next) => {
-    res.send("Hello world from Google Cloud Plataform");
+    res.send("Hello world from Google Cloud Plataform and Github Actions");
 });
 
 
